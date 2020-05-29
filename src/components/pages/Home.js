5a2dcc53900a2f8064774fc7985f709659bf12ff
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-05-29 11:06:14 -0700

    some style complete

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,46 +1,70 @@
 import React from "react";
 import image from "../../assets/image/video-list-4.jpg";
+import locationImg from "../../assets/image/noun_location_1047157.png";
+import heroImg from "../../assets/image/hero.png";
+import greyImg from "../../assets/image/grey.jpg";
+import magnify from "../../assets/image/magnify.png";
+import image1 from "../../assets/image/1.jpg";
+import image2 from "../../assets/image/2.jpg";
 
 class Home extends React.Component {
   render() {
     return (
       <>
         <div className="header">
-          <div className="farmers">
-            <h2>Farmers</h2>
+          <div className="header_title">
+            <img className="header_locationImg" src={locationImg} alt="" />
+            <h3>Toronto</h3>
           </div>
-          <div className="category">
-            <ul>
-              <li>category</li>
-              <li>category</li>
-              <li>category</li>
-            </ul>
+          <div className="searchBar">
+            <img className="searchBar-img" src={magnify} alt="" />
+            <input
+              className="searchBar-input"
+              type="text"
+              placeholder="search market"
+            />
           </div>
         </div>
 
-        <div className="main">
-          <div className="card">
-            <div className="img">
-              <img src={image} alt="" />
+        <div className="hero">
+          <img src={heroImg} alt="" />
+        </div>
+
+        <div className="nearYou">
+          <h4>Markets near you</h4>
+          <div className="container">
+            <div className="nearYou_card">
+              <div className="nearYou_card__img">
+                <img src={image1} alt="" />
+              </div>
+              <div className="nearYou_card__adress">
+                <p>Village Market</p>
+                <p>1900 betty st.</p>
+              </div>
             </div>
-            <div className="description">
-              <p>some text</p>
+            <div className="nearYou_card">
+              <div className="nearYou_card__img">
+                <img src={image2} alt="" />
+              </div>
+              <div className="nearYou_card__adress">
+                <p>Village Market</p>
+                <p>1900 betty st.</p>
+              </div>
             </div>
           </div>
-          <div className="card">
-            <div className="img">
-              <img src={image} alt="" />
-            </div>
-            <div className="description">
-              <p>some text</p>
-            </div>
-          </div>
-          <div className="card">
-            <div className="img">
-              <img src={image} alt="" />
-            </div>
-            <div className="description">
-              <p>some text</p>
+
+          <div className="subscriptions">
+            <h4>Subscriptions</h4>
+            <div className="subscriptions-cardWrapper">
+              <div className="subscriptions-card">
+                <img src={greyImg} alt="" />
+              </div>
+              <div className="subscriptions-card">
+                <img src={greyImg} alt="" />
+              </div>
+              <div className="subscriptions-card">
+                <img src={greyImg} alt="" />
+              </div>
             </div>
           </div>
         </div>
