7c0a93bb80b5d0d1936589ae3a6574680ee311b9
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-05-28 23:53:53 -0700

    add main page initial mock up based on UX team provide

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,44 @@
 import React from "react";
+import image from "../../assets/image/video-list-4.jpg";
 
 class Home extends React.Component {
   render() {
-    return <h1>home page</h1>;
+    return (
+      <>
+        <div className="header">
+          <div className="farmers">
+            <h2>Farmers filter</h2>
+          </div>
+          <div className="category">
+            <ul>
+              <li>category</li>
+              <li>category</li>
+              <li>category</li>
+            </ul>
+          </div>
+        </div>
+        <div className="main">
+          <div className="card">
+            <img src={image} alt="" />
+            <div className="description">
+              <p>some text</p>
+            </div>
+          </div>
+          <div className="card">
+            <img src={image} alt="" />
+            <div className="description">
+              <p>some text</p>
+            </div>
+          </div>
+          <div className="card">
+            <img src={image} alt="" />
+            <div className="description">
+              <p>some text</p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
